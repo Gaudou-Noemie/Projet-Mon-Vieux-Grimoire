da70bf496b67fcfab457f49504707568dcd3c320
Gaudou Noémie <gaudou.noemie@gmail.com>
2024-12-04 16:27:51 +0100

    Connexion à MongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,31 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => console.log('Connexion à MongoDB réussie !'))
+.catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+app.use(express.json());
 
 app.use((req, res, next) => {
-    console.log('Requête reçue !!');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
 
-app.use((req, res, next) => {
-    res.status(202);
-    next();
+
+app.get('/api/books', (req, res, next) => {
+    res.status(202).json();
+});
+
+app.post('/api/books',(req, res, next) => {
+    res.status(200).json({message: " Objet ajouter avec succès"});
+   
 });
 
 app.use((req, res, next) => {
